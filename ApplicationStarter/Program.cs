17cdb49469a47272372f3b9b09fd070c0568b0c4
Alex236 <alexplex236@gmail.com>
2020-06-13 19:51:02 +0300

    first commit

---
+++ ApplicationStarter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ApplicationStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processes = new List<Process>();
+            var exeNames = new string[]
+            {
+                "ServerSide.exe",
+                "ClientSide.exe",
+                "Data.exe"
+            };
+
+            foreach (var exeName in exeNames)
+            {
+                try
+                {
+                    ProcessStartInfo processStarter = new ProcessStartInfo();
+
+                    processStarter.FileName = exeName;
+                    processStarter.UseShellExecute = true;
+                    processes.Add(Process.Start(processStarter));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadKey();
+            
+            processes.ForEach(p => p.Close());
+        }
+    }
+}
